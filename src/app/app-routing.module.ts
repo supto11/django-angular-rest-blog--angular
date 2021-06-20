c89fc8e7954bf6f171f65c4be35cb6b5c208fccd
supto11 <codersupto@gmail.com>
2021-06-21 03:47:16 +0600

    setup post create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreateComponent} from "./components/posts/create/create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'posts/create', component: CreateComponent},
+  {path: '**', pathMatch: 'full', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CardComponent } from './components/shared/card/card.component';
+import { CreateComponent } from './components/posts/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    CardComponent
+    CardComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
