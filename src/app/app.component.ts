cbaaca10a862d3c2f172876f411b64d15053a18e
supto11 <codersupto@gmail.com>
2021-06-21 02:39:59 +0600

    change app prefix to rest-blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'rest-blog-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
